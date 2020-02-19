50f0f7da6149231ec1ec575a350c0405768bbff2
benk1 <kakengi_bernard@hotmail.com>
2020-02-19 09:18:51 +0200

    Updates

---
+++ precache-manifest.8a1902046286da070e8c1c672baefd23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04dc7d6c16bf08d1c38fe0cdff5fa402",
+    "url": "/React-ReduxSmallApp/index.html"
+  },
+  {
+    "revision": "604c312c21a91f3c253d",
+    "url": "/React-ReduxSmallApp/static/css/main.9098fe9e.chunk.css"
+  },
+  {
+    "revision": "3856340148d56c7f9439",
+    "url": "/React-ReduxSmallApp/static/js/2.cc68f2d0.chunk.js"
+  },
+  {
+    "revision": "604c312c21a91f3c253d",
+    "url": "/React-ReduxSmallApp/static/js/main.f2bc9202.chunk.js"
+  },
+  {
+    "revision": "21a6e0f8c20201646c81",
+    "url": "/React-ReduxSmallApp/static/js/runtime-main.5863456c.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/React-ReduxSmallApp/static/media/pokeball.f10bf078.png"
+  }
+]);
